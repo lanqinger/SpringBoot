e6600a683a9e1ad08924a002a318159760ef5d03
lanqinger <37525061+lanqinger@users.noreply.github.com>
2018-06-08 17:29:40 +0800

    使用xml文件配置编写sql

--- src/main/java/com/lhm/Application.java
+++ src/main/java/com/lhm/Application.java
@@ -4,10 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.lhm.dao"})
-@EnableScheduling
+@ComponentScan(basePackages = {"com.lhm"})
+//@EnableScheduling //定时任务配置
 public class Application
 {
     public static void main( String[] args )
--- src/main/java/com/lhm/dao/UserMapper.java
+++ src/main/java/com/lhm/dao/UserMapper.java
@@ -1,17 +1,17 @@
 package com.lhm.dao;
 
+import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.lhm.model.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import tk.mybatis.mapper.common.BaseMapper;
 
 import java.util.List;
 import java.util.Map;
 
-public interface UserMapper extends BaseMapper<User>{
-    @Select("SELECT id, account_number as accountNumber, password, create_date as createDate FROM user_info ")
+public interface UserMapper extends BaseMapper<User> {
+    //@Select("SELECT id, account_number as accountNumber, password, create_date as createDate FROM user_info ")
     List<User> listUsers(Map<String, Object> param);
 
-    @Select("SELECT count(1) FROM user_info ")
+    //@Select("SELECT count(1) FROM user_info ")
     int countRows();
 }

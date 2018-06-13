e9ffa1a59015cddde5914e6be000646c5255a618
lanqinger <37525061+lanqinger@users.noreply.github.com>
2018-06-13 09:32:01 +0800

    新增redis缓存

--- src/main/java/com/lhm/controller/UserController.java
+++ src/main/java/com/lhm/controller/UserController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  *  默认返回JSON字符串
@@ -38,6 +36,44 @@ public class UserController {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("userList", userList);
         jsonObject.put("count", count);
+        return jsonObject.toString();
+    }
+
+    @GetMapping(value="getUserById.do")
+    public String getUserById() {
+        JSONObject jsonObject = new JSONObject();
+        User user = userService.getUserById("2");
+        jsonObject.put("user", user);
+        return jsonObject.toString();
+    }
+
+    @GetMapping(value = "update.do")
+    public String update() {
+        User user = userService.getUserById("2");
+        user.setPassword("123456");
+        userService.updateUser(user);
+        User user2 = userService.getUserById("3");
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String createDate = dateTime.format(formatter);
+        user2.setCreateDate(createDate);
+        userService.updateUser(user2);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user1", user);
+        jsonObject.put("user2", user2);
+        return jsonObject.toString();
+    }
+
+    @GetMapping(value = "delete.do")
+    public String delete() {
+        List<String> ids = new ArrayList<>();
+        ids.add("4");
+        ids.add("5");
+        User user = userService.getUserById("4");
+        User user2 = userService.getUserById("5");
+        userService.deleteByIds(ids);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("ids" ,ids);
         return jsonObject.toString();
     }
 
